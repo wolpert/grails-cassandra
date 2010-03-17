7a9125339d94f2d264c5e925f0e8a345828f9920
Ned Wolpert <ned.wolpert@gmail.com>
2010-03-17 14:33:37 -0700

    Started with real test cases

--- test/unit/codehead/CassandraServiceTests.groovy
+++ test/unit/codehead/CassandraServiceTests.groovy
@@ -1,6 +1,14 @@
 package codehead;
 
 import grails.test.*
+import org.apache.cassandra.service.NotFoundException;
+
+/**
+ * mockFor requires a groovy object, where the keyspace I'm mocking is
+ * a java one. So this class is just a holder.
+ */
+class MockKeyspace {
+}
 
 class CassandraServiceTests extends GrailsUnitTestCase {
 
@@ -8,14 +16,52 @@ class CassandraServiceTests extends GrailsUnitTestCase {
 
     protected void setUp() {
         super.setUp()
-        cassandraService = new CassandraService()
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
+    void testMock() {
+        def mockKeyspaceImpl = mockFor(MockKeyspace)
+        mockKeyspaceImpl.demand.getName(1..1){ -> return "mock keyspace"}
+        def cassandraService = getCassandraService(mockKeyspaceImpl.createMock())
 
+        // run test
+        def str = cassandraService.execute{keyspace ->keyspace.getName()}
+        assertEquals("mock keyspace", str)
     }
+
+    void testExceptionCatcherWithNotFoundException(){
+        def mockKeyspaceImpl = mockFor(MockKeyspace)
+        mockKeyspaceImpl.demand.getColumn(1..1){arg1, arg2 -> throw new NotFoundException()}
+        def cassandraService = getCassandraService(mockKeyspaceImpl.createMock())
+        cassandraService.hideNotFoundExceptions=true
+        assertNull cassandraService.getColumnValue('colFamName','key','columnName')
+    }
+
+    void testExceptionCatcherWithRealException(){
+        def mockKeyspaceImpl = mockFor(MockKeyspace)
+        mockKeyspaceImpl.demand.getColumn(1..1){arg1, arg2 -> throw new Exception()}
+        def cassandraService = getCassandraService(mockKeyspaceImpl.createMock())
+        cassandraService.hideNotFoundExceptions=true
+        shouldFail{
+            cassandraService.getColumnValue('colFamName','key','columnName')
+        }
+    }
+
+    /**
+     * Returns a cassandra service that uess the keyspace for its execute
+     */
+    def getCassandraService(keyspace){
+        cassandraService = new CassandraService()
+        mockLogging(CassandraService)
+        def emc = new ExpandoMetaClass(CassandraService.class, false)
+        emc.execute = { it(keyspace)  }
+
+        emc.initialize()
+        cassandraService.metaClass = emc
+        return cassandraService
+    }
+
 }
