df66fc0dbf93ba4ad93ad817b887f8cd7f7145d3
Ned Wolpert <ned.wolpert@gmail.com>
2010-03-26 16:18:38 -0700

    Updated rev number

--- CassandraGrailsPlugin.groovy
+++ CassandraGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class CassandraGrailsPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = "0.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.1 > *"
     // the other plugins this plugin depends on
