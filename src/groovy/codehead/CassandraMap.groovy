08bd606d4f31c60feb519f7d502bd37a3c63bf41
Ned Wolpert <ned.wolpert@gmail.com>
2010-04-08 13:28:29 -0700

    Added cassandra 6.0 upgrade

---
+++ src/groovy/codehead/CassandraMap.groovy
@@ -0,0 +1,60 @@
+package codehead
+
+import java.util.Map
+
+/**
+ *
+ * @author wolpert
+ */
+class CassandraMap implements Map{
+
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean containsKey(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean containsValue(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object get(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object put(Object k, Object v) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object remove(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void putAll(Map map) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Set keySet() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Collection values() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Set entrySet() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
+
