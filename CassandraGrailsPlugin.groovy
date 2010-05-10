81e5377cbdceece9a51f44193df5c1bd3c0b416e
Ned Wolpert <ned.wolpert@gmail.com>
2010-05-10 14:41:27 -0700

    Updated to make use of new Hector client

--- CassandraGrailsPlugin.groovy
+++ CassandraGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class CassandraGrailsPlugin {
     // the plugin version
-    def version = "0.5.0"
+    def version = "0.5.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.2 > *"
     // the other plugins this plugin depends on
